a2f63e1d96d64226184a664203ff2a8d0d0f8058
Hermela-Fikre <hermelafikre3@gmail.com>
2020-04-16 09:19:53 +0300

    Defined my_each_with_index

--- main.rb
+++ main.rb
@@ -3,8 +3,14 @@ module Enumerable
         slef.length.times do |i| yield slef[i]
         end
     end
+
+    def my_each_with_index
+        self.length.times { |i| yield self[i], i }
+    end
+
 end
 
 
 # TESTS
 # [1, 2, 3, 4, 5].my_each { |item| puts item * 2 }
+# [1, 2, 3, 4, 5].my_each_with_index { |item, index| puts " #{item} and index is #{index}" }
