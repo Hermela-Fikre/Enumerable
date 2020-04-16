5309bb435582780e233cd386c667376648274105
Hermela-Fikre <hermelafikre3@gmail.com>
2020-04-16 11:22:28 +0300

    Fix stickler errors

--- main.rb
+++ main.rb
@@ -1,71 +1,71 @@
-module Enumerable  
-    def my_each
-        size.times do |i| yield self[i]
-        end
+module Enumerable
+  def my_each
+    size.times do |i|
+      yield self[i]
     end
+  end
 
-    def my_each_with_index
-        self.size.times { |i| yield self[i], i }
+  def my_each_with_index
+    size.times { |i| yield self[i], i }
+  end
+
+  def my_select
+    result_array = []
+    my_each do |i|
+      result_array << i if yield(i)
     end
+    result_array
+  end
 
-    def my_select
-        result_array = []
-        self.my_each do |i| result_array << i if yield(i) 
-        end
-        result_array
+  def my_all?
+    result = true
+    my_each { |i| result = false unless yield(i) }
+    result
+  end
+
+  def my_any?
+    result = false
+    my_each { |i| result = true if yield(i) }
+    result
+  end
+
+  def my_none?
+    result = true
+    my_each { |i| result = false if yield(i) }
+    result
+  end
+
+  def my_count
+    result = 0
+    my_each { |i| result += 1 if yield(i) }
+    result
+  end
+
+  def my_map(x = true)
+    if x == true
+      result = []
+      my_each { |i| result.push(yield(i)) }
+      result
     end
+  end
 
-    def my_all?
-        result = true
-        self.my_each { |i| result = false unless yield(i) }
-        result
+  def my_inject
+    result = first
+    my_each_with_index do |item, index|
+      next if index == 0
+
+      result = yield(result, item)
     end
+    result
+  end
 
-    def my_any?
-        result = false
-        self.my_each { |i| result = true if yield(i) }
-        result
-    end
-
-    def my_none?
-        result = true
-        self.my_each { |i| result = false if yield(i) }
-        result
-    end
-
-    def my_count
-        result = 0
-        self.my_each { |i| result += 1 if yield(i) }
-        result
-    end
-
-    def my_map (x = nil)
-        if x == nil
-            result = []
-            self.my_each { |i| result.push(yield(i)) }
-            result
-        end
-    end
-
-    def my_inject
-        result = self.first
-        self.my_each_with_index do |item, index|
-            next if index == 0
-            result = yield(result, item)
-        end
-        result
-    end
-
-    def multiply_els
-        self.my_inject { |total, item| total * item }
-    end
-
-
+  def multiply_els
+    my_inject { |total, item| total * item }
+  end
 end
 
-
 # TESTS
-# puts [1,2,3,4,5].my_each { |item| puts item * 2 }
+puts [1, 2, 3, 4, 5].my_each { |item| puts item * 2 }
 
 # [1, 2, 3, 4, 5].my_each_with_index { |item, index| puts " #{item} and index is #{index}" }
 
@@ -79,11 +79,11 @@ end
 
 # puts [1, 3, 5, 7].my_count { |item| item % 2 == 0 }
 
-puts [1, 2, 3, 4, 5].my_map { |item| item * 2 }
+# puts [1, 2, 3, 4, 5].my_map { |item| item * 2 }
 
-my_proc = Proc.new { |item| item * 5 }
+# my_proc = Proc.new { |item| item * 5 }
 
-puts [1, 2, 3, 4, 5].my_map(my_proc)
+# puts [1, 2, 3, 4, 5].my_map(my_proc)
 
 # puts [2, 2, 3, 4, 5].my_inject { |total, element| total * element }
 
