ef6beb465363d5fe75c6efcac83b5fc8d134399d
Hermela-Fikre <hermelafikre3@gmail.com>
2020-04-16 09:20:55 +0300

    Defined my_select method

--- main.rb
+++ main.rb
@@ -8,9 +8,17 @@ module Enumerable
         self.length.times { |i| yield self[i], i }
     end
 
+    def my_select
+        result_array = []
+        self.my_each do |i| result_array << i if yield(i) 
+        end
+        result_array
+    end
+
 end
 
 
 # TESTS
 # [1, 2, 3, 4, 5].my_each { |item| puts item * 2 }
 # [1, 2, 3, 4, 5].my_each_with_index { |item, index| puts " #{item} and index is #{index}" }
+# puts [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].my_select { |item| item % 2 != 0 }
