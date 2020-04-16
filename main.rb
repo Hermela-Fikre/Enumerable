06960e066318a80d702c540b264be7adb2658ca0
Hermela-Fikre <hermelafikre3@gmail.com>
2020-04-16 09:18:04 +0300

    Defined my_each method

--- main.rb
+++ main.rb
@@ -1,2 +1,10 @@
-module enumerable  
+module Enumerable  
+    def my_each
+        slef.length.times do |i| yield slef[i]
+        end
+    end
 end
+
+
+# TESTS
+# [1, 2, 3, 4, 5].my_each { |item| puts item * 2 }
